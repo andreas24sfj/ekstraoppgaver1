50e7e2e97064d6004231ab8a63c95efa3d612657
andreas24sfj <andreas24sfj@gmail.com>
2024-11-21 14:24:42 +0100

    lagt til kommentarer

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
 ﻿//1
 using System.Globalization;
-
+//lager 2 heltall
 int a = 54;
 int b = 17;
 
-
+//lager en metode som summerer to heltall
 int AddTwoNumbers(int A, int B)
 {
     return A + B;
 }
 
+//skriver ut summen av a og b
 Console.WriteLine(AddTwoNumbers(a, b));
 
 //2
+//lager et ferdig array av typen int med noen tallverdier
 int[] array = [1, 2, 4, 7, 18, 3, 8];
 
+//lager en metode som summerer alle verdiene i et array
 static int ArrayTotal(int[] C)
 {
     int sum = 0;
@@ -25,9 +28,11 @@ static int ArrayTotal(int[] C)
     }
     return sum;
 }
-
+//summerer og skriver ut summen av arrayet array
 Console.WriteLine(ArrayTotal(array));
+
 //3
+//lager et nytt dictionary myDictionary med 3 entries
 Dictionary<string, int> myDictionary = new Dictionary<string , int>()
 {
     {"Andreas", 31},
@@ -35,15 +40,18 @@ Dictionary<string, int> myDictionary = new Dictionary<string , int>()
     {"Kari", 60}
 };
 
+//lager 4 keyvaluepairs med navn og alder
 KeyValuePair<string, int> harald = new KeyValuePair<string, int>("Harald", 35);
 KeyValuePair<string, int> eva = new KeyValuePair<string, int>("Eva", 50);
 KeyValuePair<string, int> håkon = new KeyValuePair<string, int>("Håkon", 80);
-KeyValuePair<string, int> dudleif = new KeyValuePair<string, int>("dudleif", 108);
+KeyValuePair<string, int> dudleif = new KeyValuePair<string, int>("dudleif", 108);y
+//legger til entries til mydictionary, navn inn som key og alder inn som value
 myDictionary.Add(harald.Key, harald.Value);
 myDictionary.Add(eva.Key, eva.Value);
 myDictionary.Add(håkon.Key, håkon.Value);
 myDictionary.Add(dudleif.Key, dudleif.Value);
 
+//går igjennom hver entry i myDictionary og skriver ut alle verdiene
 foreach (KeyValuePair<string, int> person in myDictionary)
 {
     Console.WriteLine(person);
